bb757832f4b0e60b220911ef13e70558b55c87bb
betsamir22-design <betsamir22@gmail.com>
2026-07-11 18:21:59 +0300

    Update Calculator.py
    Version 2

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,4 @@
+mport math 
 print("#"*50)
 view="calculator"
 print(view.center(46,"="))
@@ -6,20 +7,38 @@ print("#"*50)
 
 while True:
    number1=int(input("write your first number :"))
-   operation=input("choose an operation (+,*,/,-):")
-   number2=int(input("write your second number:"))
+   operation=input("choose an operation (+,*,/,-,**,√,//,%):")
+   
 
    if operation=="+":
+       number2=int(input("write your second number :"))
        print(number1+number2)
    elif operation=="-":
-      print(number1-number2)
+       number2=int(input("write your second number :"))
+       print(number1-number2)
    elif operation=="/":
-    if number2==0:
-        print("math error, division by zero is not valid")
-    else:
-        print(number1/number2)
+       number2=int(input("write your second number :"))
+       if number2==0:
+           print("math error, division by zero is not valid")
+       else:
+          print(number1/number2)
    elif operation=="*":
+      number2=int(input("write your second number :"))
       print(number1*number2)
+   elif operation=="**":
+       number2=int(input("write your second number :"))
+       print(number1**number2)
+   elif operation=="√":
+       print(math.sqrt(number1))
+   elif operation=="//":
+       number2=int(input("write your second number :"))
+       print(number1//number2)
+   elif operation=="%":
+       number2=int(input("write your second number :"))
+       print(number1%number2)
+       
    else:
-      print("this is an invalid operation,choose(+,*,-,/):")
-    
+        print("this is an invalid operation, choose (+,-,*,/,**,√,//,%):")
+   choice=input("exit or continue ? :").lower().strip()
+   if choice in ["exit","e"]:
+        break   
